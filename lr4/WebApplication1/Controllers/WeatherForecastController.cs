9e8bab10f459ad419beda3aea3c321a3e649094f
Sofia <52317905+sonyadk@users.noreply.github.com>
2021-11-18 21:28:50 +0300

    Add files via upload

---
+++ lr4/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class WeatherForecastController : Controller
+    {
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("index")]
+        public async Task<IActionResult> Index()
+        {
+            return this.Json(new { result = "Hello World!" });
+        }
+
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("hello_world")]
+        public async Task<IActionResult> Test()
+        {
+            MyClassResponse res = new MyClassResponse();
+            res.Success = "success";
+            res.Result = "hello_world!";
+            res.Version = "1.0";
+            return this.Json(res);
+        }
+    }
+}
+
---
+++ lr4/WebApplication1/Structures/MyClassResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class MyClassResponse
+    {
+        public string Success { get; set; }
+        public string Result { get; set; }
+        public string Version { get; set; }
+
+    }
+}
